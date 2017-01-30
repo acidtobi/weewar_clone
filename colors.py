34f8e07198c1e116021cbaae7108c5cdaa00edcf
Tobi <acidtobi@gmail.com>
2017-01-30 17:58:16 +0100

    beautified

--- colors.py
+++ colors.py
@@ -1,21 +1,33 @@
-colordata = [
-    [0, "",  "none"],
-    [1, "r", "red"],
-    [2, "b", "blue"],
-    [3, "y", "yellow"],
-    [4, "g", "green"],
-    [5, "w", "white"],
-    [6, "p", "purple"]
-]
+def _reduce_list(l, col1, col2):
+    return [(x[col1], x[col2]) for x in l]
+
 
 class Color(object):
-    def __init__(self, id, code, name):
-        self.id = id
-        self.code = code
-        self.name = name
 
-type = [Color(id, code, name) for (id, code, name) in colordata]
-code2id = dict((code, id) for (id, code, name) in colordata)
+    colordata = [
+        [0, "",  "none"],
+        [1, "r", "red"],
+        [2, "b", "blue"],
+        [3, "y", "yellow"],
+        [4, "g", "green"],
+        [5, "w", "white"],
+        [6, "p", "purple"]
+    ]
 
-def id_from_code(color_code):
-    return code2id[color_code]
+    color_id = dict(_reduce_list(colordata, 1, 0) + _reduce_list(colordata, 2, 0))
+    color_code = dict(_reduce_list(colordata, 0, 1) + _reduce_list(colordata, 2, 1))
+    color_name = dict(_reduce_list(colordata, 0, 2) + _reduce_list(colordata, 1, 2))
+
+    @staticmethod
+    def id(key):
+        return Color.color_id[key]
+
+    @staticmethod
+    def code(key):
+        return Color.color_code[key]
+
+    @staticmethod
+    def name(key):
+        return Color.color_name[key]
+
+
